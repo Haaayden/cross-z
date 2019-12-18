ef8e5b28cc6fa7e683e17619a83cd7b55e2bde51
Haaayden <brooklyn1984@qq.com>
2019-12-18 11:49:03 +0800

    :sparkles: 从排序数组中删除重复元素

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let pkg = require('./src/easy-1')
 
-const res = pkg([0,0,1,1,1,2,2,3,3,4])
+// let testCase = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+let testCase = [0]
+const res = pkg(testCase)
 
 console.log('res', res)
+console.log('testCase', testCase)
--- src/easy-1.js
+++ src/easy-1.js
@@ -1,17 +1,35 @@
+// 从排序数组中删除重复项 [26]
 // https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/21/
-// 从排序数组中删除重复项
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
+// function removeDuplicates(nums) {
+//   if (nums.length <= 1) return nums.length
+
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     if (i - 1 >= 0 && nums[i - 1] === nums[i]) {
+//       nums.splice(i, 1)
+//     }
+//   }
+//   return nums.length
+// }
+
+// 双指针
 function removeDuplicates(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    if (i > 0 && nums[i] === nums[i-1]) {
-      nums.splice(i, 1)
+  if (nums.length <= 1) return nums.length
+
+  let i = 0   // 慢指针
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++
+      nums[i] = nums[j]
     }
   }
-  return nums
+  nums.splice(i + 1)
+  return i + 1
 }
+
 
 module.exports = removeDuplicates
