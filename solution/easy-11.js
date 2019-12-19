8e911e1e0730296c45dde33dfd0f43c5b3e54ee4
Haaayden <brooklyn1984@qq.com>
2019-12-19 22:14:26 +0800

    :sparkles: 旋转图像

---
+++ solution/easy-11.js
@@ -0,0 +1,22 @@
+// 旋转图像 [48]
+// https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/31/
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  let tmp
+  let length = matrix.length
+
+  for (i = 0; i < parseInt(length / 2); i++) {
+    for (j = i; j < length - 1 - i; j++) {
+      tmp = matrix[i][j]
+      matrix[i][j] = matrix[length - j - 1][i]
+      matrix[length - j - 1][i] = matrix[length - i - 1][length - j - 1]
+      matrix[length - i - 1][length - j - 1] = matrix[j][length - i - 1]
+      matrix[j][length - i - 1] = tmp
+    }
+  }
+}
+module.exports = rotate
