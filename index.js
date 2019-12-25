9925acb3f1d7a4d16859fbe45e625f3ec9fa7199
Haaayden <brooklyn1984@qq.com>
2019-12-25 16:15:25 +0800

    :sparkles: 104-二叉树的最大深度

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { singleLinkedList } = require('./util/ListNode')
 const { getBinaryTreeFromArray } = require('./util/TreeNode')
-let solution = require('./solution/easy-26')
+let solution = require('./solution/easy-27')
 
-let testCase = getBinaryTreeFromArray([3,9,20,null,null,15,7])
+// let testCase = getBinaryTreeFromArray([3,9,20,null,null,15,7])
+let testCase = getBinaryTreeFromArray([])
 // let testCase2 = singleLinkedList([1, 3, 4])
-// const res = solution(testCase)
+const res = solution(testCase)
 
 console.log('testCase', testCase)
 console.log('res', JSON.stringify(res))
--- solution/easy-27.js
+++ solution/easy-27.js
@@ -1,4 +1,4 @@
-// 二叉树的最大深度
+// 二叉树的最大深度 [104]
 // https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/47/
 
 /**
@@ -9,9 +9,40 @@
  * }
  */
 /**
+ * 迭代
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
+// var maxDepth = function(root) {
+//   if (root === null) return 0
+//   let stack = [ new Map([[root, 1]]) ]
+//   let depth = 0
+//   while (stack.length !== 0) {
+//     let curNodeMap = stack.pop()
+//     let curNode
+//     let curDepth
+//     for (let [key, value] of curNodeMap) {
+//       curNode = key
+//       curDepth = value
+//     }
+//     if (curNode.val !== null) {
+//       depth = Math.max(depth, curDepth)
+//       if (curNode.left && curNode.left.val !== null) stack.push(new Map([[curNode.left, curDepth + 1]]))
+//       if (curNode.right && curNode.right.val !== null) stack.push(new Map([[curNode.right, curDepth + 1]]))
+//     }
+//   }
+//   return depth
+// }
 
+// 递归
+var maxDepth = function(root) {
+  if (root === null) {
+    return 0
+  } else {
+    let left_depth = maxDepth(root.left)
+    let right_depth = maxDepth(root.right)
+    return Math.max(left_depth, right_depth) + 1
+  }
 }
+
+module.exports = maxDepth
