6348394f539ffc74b5cdbdaac86ee04912d157f8
Haaayden <brooklyn1984@qq.com>
2019-12-26 11:08:43 +0800

    :sparkles: 102-二叉树的层次遍历

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { singleLinkedList } = require('./util/ListNode')
 const { getBinaryTreeFromArray } = require('./util/TreeNode')
-let solution = require('./solution/easy-29')
+let solution = require('./solution/easy-30')
 
 // let testCase = getBinaryTreeFromArray([3,9,20,null,null,15,7])
-let testCase = getBinaryTreeFromArray([10, 5, 15, null, null, 6, 20])
+let testCase = getBinaryTreeFromArray([])
 // let testCase2 = singleLinkedList([1, 3, 4])
 const res = solution(testCase)
 
--- solution/easy-30.js
+++ solution/easy-30.js
@@ -1,4 +1,4 @@
-// 二叉树的层序遍历
+// 二叉树的层次遍历 [102]
 // https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/50/
 
 /**
@@ -12,8 +12,27 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// 迭代
 var levelOrder = function(root) {
+  if (root === null) return []
+  let queue = [root]
+  let level = 0
+  let res = []
+  while (queue.length !== 0) {
+    res[level] = []
 
+    let queueLength = queue.length
+    for (let i = 0; i < queueLength; i++) {
+      let node = queue.shift()
+      res[level].push(node.val)
+
+      if (node.left !== null) queue.push(node.left)
+      if (node.right !== null) queue.push(node.right)
+    }
+
+    level++
+  }
+  return res
 }
 
 module.exports = levelOrder
